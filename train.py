8a3302543a5ad4400be1efa140804a555153a8bd
huyhoan1109 <huyhoan0901@gmail.com>
2024-03-07 08:49:21 +0700

    add kaggle pretrain path

--- train.py
+++ train.py
@@ -182,8 +182,7 @@ def main_worker(gpu, args):
         train_sampler.set_epoch(epoch_log)
 
         # train
-        train(train_loader, model, optimizer, scheduler, scaler, epoch_log,
-              args)
+        train(train_loader, model, optimizer, scheduler, scaler, epoch_log, args)
 
         # evaluation
         iou, prec_dict = validate(val_loader, model, epoch_log, args)
