dd92edbc0d9cc4b5ca4be2a75b9eb586d27b75f6
unknown <huyhoan0901@gmail.com>
2024-03-11 23:38:34 +0700

    edit model projector

--- model/cris.py
+++ model/cris.py
@@ -26,7 +26,10 @@ class CRIS(nn.Module):
                                           dropout=cfg.dropout,
                                           return_intermediate=cfg.intermediate)
         # Projector
-        self.proj = Projector(cfg.word_dim, cfg.vis_dim // 2, 3)
+        if cfg.projector == 2:
+            self.proj = Projector2(cfg.word_dim, cfg.vis_dim // 2, 3)
+        else:
+            self.proj = Projector(cfg.word_dim, cfg.vis_dim // 2, 3)
         loss_type = cfg.loss_type
         if loss_type == 'focal':
             self.loss_func = FocalLoss(cfg)
