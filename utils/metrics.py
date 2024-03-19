1fab0720fc749f5a7dd07b54f5cb9812d89d5f08
huyhoan1109 <huyhoan0901@gmail.com>
2024-03-19 15:14:11 +0700

    fix metrics

--- utils/metrics.py
+++ utils/metrics.py
@@ -1,9 +1,15 @@
 def calculate_metrics(pred, target, dim, smooth=1e-6, ths=0.5):
     union = (pred.bool() | target.bool()).sum(dim=dim)  
-    tp = union[(target == 1) & (pred == 1)]
-    tn = union[(target == 0) & (pred == 0)]
-    fp = union[(target == 1) & (pred == 0)]
-    fn = union[(target == 0) & (pred == 1)]
+    
+    tp_mask = (target == 1) & (pred == 1)
+    tn_mask = (target == 0) & (pred == 0)
+    fp_mask = (target == 1) & (pred == 0)
+    fn_mask = (target == 0) & (pred == 1)
+    
+    tp = union[tp_mask]
+    tn = union[tn_mask]
+    fp = union[fp_mask]
+    fn = union[fn_mask]
     
     ious = (tp+smooth)/(tp+tn+smooth)
     iou = ious.mean()
