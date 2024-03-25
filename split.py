a684f6403130b5239a0fe0c6f985202734c5d3c7
huyhoan1109 <huyhoan0901@gmail.com>
2024-03-25 17:39:43 +0700

    delete split.py

--- split.py
+++
@@ -1,7 +0,0 @@
-polyp_dir = '/mnt/ducntm/endoscopy/polyp_dai_truc_trang_20230712'
-ut_da_day_dir = '/mnt/ducntm/endoscopy/ung_thu_da_day_20230620'
-ut_thuc_quan_dir = '/mnt/ducntm/endoscopy/ung_thu_thuc_quan_20230620'
-viem_da_day_dir = '/mnt/ducntm/endoscopy/viem_da_day_20230620'
-viem_loet_hoanh_ta_trang_dir = '/mnt/ducntm/endoscopy/viem_loet_hoanh_ta_trang_20230620'
-viem_thuc_quan_dir = '/mnt/ducntm/endoscopy/viem_thuc_quan_20230620'
-
