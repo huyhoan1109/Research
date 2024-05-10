1e3b951fde7b3f131bbef575a0942fd83f7f5848
huyhoan1109 <huyhoan0901@gmail.com>
2024-05-10 18:47:30 +0700

    update bash

--- bash/train_endo_r101.sh
+++ bash/train_endo_r101.sh
@@ -3,4 +3,4 @@ CUDA_VISIBLE_DEVICES=$1 python \
     --config config/endo/cris_r101.yaml \
     --root_data /mnt/tuyenld/data/endoscopy/full_endo_data \
     --tsg $2 \
-    --opts TRAIN.exp_name CRIS_R101_ENDO TRAIN.clip_pretrain TRAIN.clip_pretrain exp/endo-RN101_best_model.pt
+    --opts TRAIN.exp_name CRIS_R101_ENDO TRAIN.clip_pretrain exp/endo-RN101_best_model.pt
